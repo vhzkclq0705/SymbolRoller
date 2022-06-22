45ead724230c4a6b454f41802364899010f52866
o_jun <vhzkclq0705@naver.com>
2022-06-22 22:47:32 +0900

    Implementation completed with Storyboard

--- SymbolRoller/ViewController.swift
+++ SymbolRoller/ViewController.swift
@@ -8,15 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var symbolImageView: UIImageView!
+    @IBOutlet weak var symbolLabel: UILabel!
+    @IBOutlet weak var reloadButton: UIButton!
+    
+    let symbols = ["cloud", "sun.min", "wind", "cloud", "moon", "snowflake"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Commit test
-        // Commit test// Commit test
-        // Commit test
-        // Commit test
+        reloadButton.layer.cornerRadius = reloadButton.bounds.height / 2
+        reloadItems()
     }
-
-
 }
 
+extension ViewController {
+    func reloadItems() {
+        let num = Int.random(in: 0...symbols.count - 1)
+        symbolImageView.image = UIImage(systemName: symbols[num])
+        symbolLabel.text = symbols[num]
+    }
+    
+    @IBAction func didTapReloadButton(_ sender: Any) {
+        reloadItems()
+    }
+}
